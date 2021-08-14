fa79272c985d73be297832332f1133d40c2f15fc
unknown <justink6@illinois.edu>
2021-08-14 16:06:24 -0500

    unnecessary files

--- backend/index.js
+++ backend/index.js
@@ -12,46 +12,6 @@ var db = mysql.createConnection({
     database:'discoveat',
 })
 
-// host: '35.202.192.135',
-//     user: 'root',
-//     password: 'group99',
-//     database : 'discoveat'
-
-// var db = mysql.createConnection({
-//     host: '35.202.192.135',
-//     port: '3306',
-//     user: 'root',
-//     password: 'group99',
-//     database: 'discoveat',
-//     ssl: {
-//         ca: fs.readFileSync('./server-ca.pem'),
-//         key: fs.readFileSync('./client-key.pem'),
-//         cert: fs.readFileSync('./client-cert.pem')
-//     }
-// })
-
-// const sqlSelect = "SELECT * FROM Users LIMIT 3";
-// db.query(sqlSelect, (err, result) => {
-//     console.log(result);
-//     console.log(err);
-// });
-
-// db.connect(function(err) {
-//     if (err) throw err;
-//     var sql = "INSERT INTO `movie_reviews` (`id`,`movieName`, `movieReview`) VALUES (5,'inception', 'good movie');";
-//     db.query(sql, function (err, result) {
-//       if (err) throw err;
-//       console.log(result.affectedRows + " record(s) updated");
-//     });
-//   });
-
-// app.get('/', (require, response) => {
-//     const sqlInsert = "INSERT INTO `movie_reviews` (`movieName`, `movieReview`) VALUES ('Spider2', 'good movie');";
-//     db.query(sqlInsert, (err, result) => {
-//         response.send("Hello world!!!");
-//     })
-// })
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
